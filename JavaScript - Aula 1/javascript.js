d7a27fe3d74b22bc4c05f538697d58b6c2a318fc
Victor Gonçalves <victor.algonca@gmail.com>
2024-04-03 20:58:38 -0300

    Aula 1 Finalizada

---
+++ JavaScript - Aula 1/javascript.js
@@ -0,0 +1,14 @@
+alert("Olá, cuzão!");
+/* prompt("Digite seu nome");
+prompt("Digite sua idade");
+prompt("Digite sua profissão");
+confirm("Você deseja sair ?"); */
+
+//Entrada
+var nome = prompt("Digite seu nome");
+var profissão = prompt("Digite sua profissão");
+var idade = prompt("Digite sua idade");
+
+//Saída
+console.log(`Olá ${nome}, sua idade é ${idade} e sua profissão é ${profissão}`);
+
